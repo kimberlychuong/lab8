a34d453da40e11380e777871cd351d986dd9cfa1
kimberlychuong <kichuong@ucsd.edu>
2021-02-24 18:40:27 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'hVQfzk5pVzhDeH4jKfuGtQqwt6TIUEkr';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	  center: [32.87895159458153, -117.23585797952958],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom: 12,
+	  zoomControl: false
+	});
+
+	// add a marker to the map
+	L.marker([32.87895159458153, -117.23585797952958]).addTo(map);
 }
